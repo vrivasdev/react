11b8b634fd853d4cf884db4ac4c24739394200d0
Victor Rivas <vrivas@teravisiontech.com>
2018-10-19 09:52:24 -0400

    ReactJS styles

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends Component {
 	render() {
+		/*const styles = {
+			container: {
+				fontSize: 14,
+				color: '#44546b',
+				cursor: 'pointer',
+				width: 260,
+				border: '1px solid red'
+			}
+		}*/
 		return (
-			<div>
-				<div>
-					<img src="" 
-					alt=""
-					width={260}
-					height ={160}
-					/>
-					<h3>¿Por qué aprender React?...</h3>
-					<p>Víctor Rivas</p>	
+			<div className="Media">
+				<div className="Media-cover">
+					<img className="Media-image"
+						src="./images/covers/bitcoin.jpg"
+						alt=""
+						width={260}
+						height ={160}
+						/>
+					<h3 className="Media-title">¿Por qué aprender React?</h3>
+					<p className="Media-author">Víctor Rivas</p>
 				</div>
-			</div>			
+			</div>
 		)
 	}
 }
