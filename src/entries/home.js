7a53ce6bd53be9ac6a56de0e9a3efa19b1dda3ec
Victor Rivas <vrivas@teravisiontech.com>
2018-10-23 14:51:30 -0400

    Project Structure - Containers & UI components: Categories

--- src/entries/home.js
+++ src/entries/home.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Home from '../pages/containers/home';
-//import data from '../api.json';
+import data from '../api.json';
 
 const app = document.getElementById('app')
 
-render(<Home/>, app); // renderizar
+render(<Home data={data} />, app); // renderizar
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,44 +1,16 @@
 import React from 'react';
 
 import Media from './media';
-import Play from '../../icons/components/play';
-import Volume from '../../icons/components/volume';
-import Pause from '../../icons/components/pause';
 
 import './playlist.css';
 import './category.css';
 
 function Playlist(props) {
-    const {categories} = props.data
-    return (
-      <div>
+    return(
+      <div className="Playlist">
         {
-          categories.map(category => {
-            return(
-              <div key={category.id} className="Category">
-                <h2>{category.title}</h2>
-                <p>{category.description}</p>
-                <div className="Playlist">
-                  <Play
-                    size={50}
-                    color="red"
-                  />
-                <Volume
-                  size={50}
-                  color="blue"
-                />
-              <Pause
-                  size={50}
-                  color="green"
-                />
-                {
-                  category.playlist.map(item => {
-                    return <Media {...item} key={item.id}/>
-                  })
-                }
-                </div>
-              </div>
-            )
+          props.playlist.map(item => {
+            return <Media {...item} key={item.id}/>
           })
         }
       </div>
