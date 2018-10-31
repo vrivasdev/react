679ed11c8ea7e79585e28128b8ced625d3468457
Victor Rivas <vrivas@teravisiontech.com>
2018-10-31 18:10:43 -0400

    Play/Pause: UI Creation

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,14 +2,27 @@ import React, {Component} from 'react';
 import VideoPlayerLayout from '../components/video-player-layout';
 import Video from '../components/video';
 import Title from '../components/title';
+import PlayPause from '../components/play-pause';
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true,
+  }
+  togglePlay = (event) => { // It changes every time the user clicks
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
   render() {
     return (
       <VideoPlayerLayout>
         <Title
           title="Esto es un video"
         />
+      <PlayPause
+        pause={this.state.pause}
+        handleClick={this.togglePlay}
+      />
         <Video
           autoplay={true}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
