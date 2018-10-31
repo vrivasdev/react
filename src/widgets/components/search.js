c79df49fa527c7c07010babfe15fa9476b9de488
Victor Rivas <vrivas@teravisiontech.com>
2018-10-31 11:52:32 -0400

    HTML and form events references

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './search.css';
-/*function Search(proprs) {
-  return (
-    <form action=""></form>
-  )
-}*/
 
-const Search = () => ( // => pharentesis replaces the return
-  <form action="" className="Search">
+const Search = (props) => ( // => pharentesis replaces the return
+  <form
+  className="Search"
+  onSubmit={props.handleSubmit}
+  >
     <input
+      ref={props.setRef}
       type="text"
       placeholder=""
       className="Search-input"
+      name="search"
+      onChange={props.handleChange}
+      value={props.value}
     />
   </form>
 )
